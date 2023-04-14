a71a978ad6c18dc870781d99d474b6485e55ed36
Novikov_VK <Novikov_VK@akelon.com>
2023-04-14 14:09:11 +0300

    Добавил справочник PreparedSolution

---
+++ Akelon.SolutionStorage/Akelon.SolutionStorage.ClientBase/PreparedSolution/PreparedSolutionActions.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Akelon.SolutionStorage.PreparedSolution;
+
+namespace Akelon.SolutionStorage.Client
+{
+}

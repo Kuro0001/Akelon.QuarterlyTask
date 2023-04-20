aada7589e757e95fdeb071a0e6d764f0770240b3
Novikov_VK <Novikov_VK@akelon.com>
2023-04-20 16:34:39 +0300

    Вынес поля на форму

---
+++ Akelon.HelpDesk/Akelon.HelpDesk.Server/InternalAddendumRequest/InternalAddendumRequestHandlers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Akelon.HelpDesk.InternalAddendumRequest;
+
+namespace Akelon.HelpDesk
+{
+  partial class InternalAddendumRequestServerHandlers
+  {
+
+    public override void BeforeSave(Sungero.Domain.BeforeSaveEventArgs e)
+    {
+      base.BeforeSave(e);
+      
+      var employees = Akelon.StudySolution.Employees.GetAll();
+      var dudes = employees.Where(emp => emp.IsDudeAkelon.HasValue && emp.IsDudeAkelon.Value);
+      
+      foreach (var dude in dudes)
+      {
+        _obj.AccessRights.Grant(dude, DefaultAccessRightsTypes.Read);
+      }
+    }
+  }
+}

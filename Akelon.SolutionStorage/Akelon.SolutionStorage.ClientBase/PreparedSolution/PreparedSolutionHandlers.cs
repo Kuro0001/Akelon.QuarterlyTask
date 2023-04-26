3e405bfc4a222e78baea26c2928a5af6cfb379e0
Novikov_VK <Novikov_VK@akelon.com>
2023-04-26 17:23:14 +0300

    Добавил логику проверки типа решения

--- Akelon.SolutionStorage/Akelon.SolutionStorage.ClientBase/PreparedSolution/PreparedSolutionHandlers.cs
+++ Akelon.SolutionStorage/Akelon.SolutionStorage.ClientBase/PreparedSolution/PreparedSolutionHandlers.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -9,6 +9,36 @@ namespace Akelon.SolutionStorage
 {
   partial class PreparedSolutionClientHandlers
   {
+
+    public override void Showing(Sungero.Presentation.FormShowingEventArgs e)
+    {
+       
+    }
+
+    public virtual void TypeValueInput(Sungero.Presentation.EnumerationValueInputEventArgs e)
+    {
+      if (e.NewValue != null && e.NewValue != e.OldValue)
+      {
+        if (e.NewValue.Equals(PreparedSolution.Type.Complete))
+        {
+          _obj.CodeExampleUrl = null;
+          _obj.State.Properties.CodeExampleUrl.IsVisible = false;
+          
+          _obj.State.Properties.SolutionDocument.IsVisible = true;
+          _obj.State.Properties.Package.IsVisible = true;
+        }
+        else if (e.NewValue.Equals(PreparedSolution.Type.Incomplete))
+        {
+          _obj.State.Properties.CodeExampleUrl.IsVisible = true;
+          
+          _obj.SolutionDocument.Clear();
+          _obj.Package.Clear();
+          
+          _obj.State.Properties.SolutionDocument.IsVisible = false;
+          _obj.State.Properties.Package.IsVisible = false;
+        }
+      }
+    }
 
   }
 }
